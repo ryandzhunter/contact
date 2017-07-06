685efb1e3d7976c158817f3531026595ace15b3d
aryandi2712 <aryandi.putra@mataharimall.com>
2017-07-06 17:08:17 +0700

    [ADD] Add validation on add contact activity

--- app/src/main/java/com/ryandzhunter/contact/addcontact/AddContactViewModel.java
+++ app/src/main/java/com/ryandzhunter/contact/addcontact/AddContactViewModel.java
@@ -2,15 +2,22 @@ package com.ryandzhunter.contact.addcontact;
 
 import android.content.Context;
 import android.databinding.BaseObservable;
+import android.databinding.Bindable;
 import android.databinding.BindingAdapter;
+import android.databinding.ObservableBoolean;
 import android.databinding.ObservableField;
+import android.text.TextUtils;
+import android.util.Patterns;
 import android.widget.ImageView;
 
+import com.android.databinding.library.baseAdapters.BR;
 import com.bumptech.glide.Glide;
 import com.ryandzhunter.contact.ILifecycleViewModel;
 import com.ryandzhunter.contact.R;
 import com.ryandzhunter.contact.data.model.Contact;
 import com.ryandzhunter.contact.usecase.GetContactListUseCase;
+
+import java.util.regex.Pattern;
 
 import io.reactivex.disposables.CompositeDisposable;
 
@@ -26,6 +33,11 @@ public class AddContactViewModel extends BaseObservable implements ILifecycleVie
 
     public Contact contact;
     private CompositeDisposable compositeDisposable = new CompositeDisposable();
+
+    public ObservableField<Boolean> isValidFirstName = new ObservableField<>();
+    public ObservableField<Boolean> isValidLastName = new ObservableField<>();
+    public ObservableField<Boolean> isValidPhoneNumber = new ObservableField<>();
+    public ObservableField<Boolean> isValidEmail = new ObservableField<>();
 
     public AddContactViewModel(Context context, GetContactListUseCase usecase, AddContactView view) {
         this.context = context;
@@ -43,6 +55,10 @@ public class AddContactViewModel extends BaseObservable implements ILifecycleVie
 
     public void setContact(Contact contact) {
         this.contact = contact;
+        isValidFirstName.set(isValidName(contact.firstName));
+        isValidLastName.set(isValidName(contact.lastName));
+        isValidPhoneNumber.set(isValidPhoneNumber(contact.phoneNumber));
+        isValidEmail.set((isValidEmail(contact.email)));
     }
 
     public String getFirstName() {
@@ -67,18 +83,22 @@ public class AddContactViewModel extends BaseObservable implements ILifecycleVie
 
     public void setFirstName(String firstName) {
         contact.firstName = firstName;
+        isValidFirstName.set(isValidName(firstName));
     }
 
     public void setLastName(String lastName) {
         contact.lastName = lastName;
+        isValidLastName.set(isValidName(lastName));
     }
 
     public void setEmail(String email) {
         contact.email = email;
+        isValidEmail.set(isValidEmail(email));
     }
 
     public void setPhoneNumber(String phoneNumber) {
         contact.phoneNumber = phoneNumber;
+        isValidPhoneNumber.set(isValidPhoneNumber(phoneNumber));
     }
 
     public void setProfilePic(String profilePic) {
@@ -87,6 +107,10 @@ public class AddContactViewModel extends BaseObservable implements ILifecycleVie
 
     public void onPhotoClick() {
 
+    }
+
+    public void onSaveClicked(){
+       
     }
 
     @BindingAdapter(value = {"bind:imageUrl"}, requireAll = false)
@@ -103,4 +127,22 @@ public class AddContactViewModel extends BaseObservable implements ILifecycleVie
     public void onDestroy() {
         compositeDisposable.clear();
     }
+
+    private boolean checkValidation(Contact contact) {
+        return isValidName(getFirstName()) && isValidName(getLastName())
+                && isValidPhoneNumber(getPhoneNumber()) && isValidEmail(getEmail());
+    }
+
+    public boolean isValidName(String name){
+        return !TextUtils.isEmpty(name) && name.length() > 3;
+    }
+    public boolean isValidPhoneNumber(String phone) {
+        return !TextUtils.isEmpty(phone) && Patterns.PHONE.matcher(phone).matches() && phone.length() >= 10;
+    }
+
+    public boolean isValidEmail(String email) {
+        return !TextUtils.isEmpty(email) && Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+
 }
